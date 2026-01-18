84102ba696de9c343cfc6af2238d49b3b28fc311
tigrbn <asutaiiiga@gmail.com>
2026-01-18 19:53:29 +0900

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/formoms-app/', // ← Эта строка ОБЯЗАТЕЛЬНА
   plugins: [react()],
   server: {
     port: 5173,
